5e42866acd7bb8834e266f15f34fb64d7c4a6a19
BeingmuqhtadeerM <muhammedmuzawar9@gmail.com>
2025-10-25 10:14:41 +0530

    added setup Randon Color Generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Accordion from "./components/Accordion";
+import RandomColor from "./components/Random-Color";
 
 function App() {
   return (
-    <>
+    <div className="app">
       {/* Accordion-Functionality */}
       <Accordion />
-    </>
+
+      {/* Generate Random Color */}
+      <RandomColor />
+    </div>
   );
 }
 
---
+++ src/components/Random-Color/index.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+// this wiill generate hex, rgb, etc
+export default function RandomColor() {
+  return (
+    <div className="container">
+      <button>Generate Random Color</button>
+    </div>
+  );
+}
